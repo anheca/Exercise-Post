762d550681df37f4802c337c89928e757bec836e
anheca <anheca68@hotmail.com>
2016-05-16 20:10:14 -0500

    Exercise POST
    Post

---
+++ Post.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcerciseTwoPost
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime OnCreated { get; private set; }
+
+        private int _vote;
+
+        // public propertie VOTOS using encapsulation 
+        public int Votos
+        {
+            get { return _vote;}
+            private set
+            {
+                if (value < 0)
+                    _vote = 0;
+                else
+                    _vote = value;
+            }
+
+        }
+
+        public Post()
+        {
+            OnCreated = DateTime.Now;
+            Votos = 0;
+        }
+
+        public void UpVote()
+        {
+            Votos++;
+        }
+
+        public void DownVote()
+        {
+            Votos--;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcerciseTwoPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var post = new Post();
+            post.Title = "New Post";
+            post.Description = "Fisrt exercise using encapsulation.";
+
+
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+
+            DisplayVotos(post);
+            Console.ReadKey();
+
+            post.DownVote();
+            post.DownVote();
+            post.DownVote();
+            post.DownVote();
+            post.DownVote();
+            post.DownVote();
+            post.DownVote();
+            post.DownVote();
+            post.DownVote();
+
+            DisplayVotos(post);
+            Console.ReadKey();
+        }
+
+        private static void DisplayVotos(Post post)
+        {
+            Console.WriteLine("Title      : {0}", post.Title);
+            Console.WriteLine("Description: {0}", post.Description);
+            Console.WriteLine("Total Votos: {0}", post.Votos);
+        }
+    }
+}
